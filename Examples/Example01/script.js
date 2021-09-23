b5279912d322e5d3b3504a475ced5f7d6a744663
Alvaro Ramirez <daw.alvaro@gmail.com>
2021-09-23 08:45:38 +0200

    Actualizando

---
+++ Examples/Example01/script.js
@@ -0,0 +1,11 @@
+function init(){
+    //Añadiendo listeners
+    document.getElementById("pulsador").addEventListener("click", function () {
+        cambiarTexto();
+    });
+
+}
+
+function cambiarTexto() {
+    document.getElementById('texto').innerHTML = "<p style='color:black'><h1>¡Hola mundo!</h1></p>";
+}
